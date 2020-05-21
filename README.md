c1f520b88c3d815c7ba165717fe5a2cecf3c2f28
Blackbird <ksilva.rod@gmail.com>
2020-05-21 16:55:00 -0500

    first hw attempt

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # PasswordGenerator
-Create a PG using JS
+So, in order to build my PG I started by pseudocoding the criteria I was asked to follow. I created variables and conditionals to test if the logic would work. After that, I added the function to generate the password. I made a "while loop" to show an alert until the criteria for the password length would be met. Then, I added the conditionals for the rest of the requested criteria (numbers, special characters, upper and lowercase letters, made another loop.
+For the last part I gave the variables a value and conditionals.
---
+++ script.js
@@ -0,0 +1,59 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+alert(generateBtn);
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// Write Password Function
+function generatePassword() {
+  var passwordLength= prompt("Enter the password length (Enter a number between 8-128)");
+  var passwordLengthBad = true;
+
+
+  while(passwordLengthBad) {
+      if (passwordLength>=8 && passwordLength<=128) {
+          alert("Password length is good.");
+          passwordLengthBad = false;
+      }
+      else {
+          alert("Remember your password length must be between the numbers 8 and 128 ONLY.")
+          passwordLength= prompt("Enter the password length (Enter a number between 8-128)");
+      }
+  }
+  
+  var lowercaseWanted= confirm("Would you like your password to have lowercase letters?");
+  var uppercaseWanted= confirm("Would you like your password to have uppercase letters?");
+  var numbersWanted=confirm("Would you like your password to have numbers?");
+  var specialsWanted=confirm("Would you like to have special characters in your password?");
+
+  var charactersBad = true;
+
+  while(charactersBad) {
+
+      if (lowercaseWanted || uppercaseWanted || numbersWanted || specialsWanted) {
+          alert("At least one of the characters options has been selected.");
+          charactersBad = false;
+      }
+      else {
+          alert("Select at least one of the character options.");
+          lowercaseWanted= confirm("Would you like your password to have lowercase letters?");
+          uppercaseWanted= confirm("Would you like your password to have uppercase letters?");
+          numbersWanted=confirm("Would you like your password to have numbers?");
+          specialsWanted=confirm("Would you like to have special characters in your password?");
+      }
+  }
+
+
+}
